1fbe62bdbe5dc9848495153c9e6842c3eb50ba30
EA-flip2 <91549472+EA-flip2@users.noreply.github.com>
2023-06-17 23:51:28 +0000

    Added the likes functinality

--- lib/screens/components/previous_post.dart
+++ lib/screens/components/previous_post.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firetrial/screens/components/flaged.dart';
+import 'package:firetrial/screens/components/likebutton.dart';
 import 'package:flutter/material.dart';
 
 class Posted_quest extends StatefulWidget {
@@ -8,12 +9,14 @@ class Posted_quest extends StatefulWidget {
   final String user;
   final String postId; // to identify post
   final List<String> flages; // keep track of flags
+  final List<String> likes;
   const Posted_quest({
     super.key,
     required this.message,
     required this.user,
     required this.flages,
     required this.postId,
+    required this.likes,
   });
 
   @override
@@ -24,11 +27,15 @@ class _Posted_questState extends State<Posted_quest> {
   //user
   final currentUser = FirebaseAuth.instance.currentUser!;
   bool isflagged = false;
+  bool isLiked = false;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    isflagged = widget.flages.contains(currentUser.email);
+    isflagged =
+        widget.flages.contains(currentUser.email); // keepig track of users flag
+    isLiked = widget.likes
+        .contains(currentUser.email); // keep track of number of likes
   }
 
   // toggle flagged
@@ -52,6 +59,27 @@ class _Posted_questState extends State<Posted_quest> {
     }
   }
 
+  // toggle like
+  void togglelike() {
+    setState(() {
+      isLiked = !isLiked;
+    });
+    // access document in fire base
+    DocumentReference postRef =
+        FirebaseFirestore.instance.collection("User Post").doc(widget.postId);
+
+    if (isLiked) {
+      // add user to liked field
+      postRef.update({
+        'likes': FieldValue.arrayUnion([currentUser.email])
+      });
+    } else {
+      postRef.update({
+        'likes': FieldValue.arrayRemove([currentUser.email])
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -61,9 +89,11 @@ class _Posted_questState extends State<Posted_quest> {
             leading: Icon(Icons.person),
             title: Text(widget.user),
             subtitle: Text(widget.message),
-            trailing: IconButton(
-              icon: Icon(Icons.thumb_up_sharp),
-              onPressed: () {},
+            trailing: Column(
+              children: [
+                likeButton(isLiked: isLiked, onTap: togglelike),
+                Text(widget.likes.length.toString()),
+              ],
             ),
           ),
         ),
