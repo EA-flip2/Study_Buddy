fe9494fb5ef6a3e13f5cb467ba839baec2b69aa5
Millie010 <98983021+Millie010@users.noreply.github.com>
2023-08-02 11:41:40 +0000

    reverting the body back to its orginal state

--- lib/authentication/auth.dart
+++ lib/authentication/auth.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firetrial/authentication/login_reg.dart';
-import 'package:firetrial/screens/body/navigator_.dart';
-import 'package:flutter/material.dart';
+
 
 class Auth_Page extends StatelessWidget {
-  const Auth_Page({super.key});
+  const Auth_Page({Key? key}); // Add the 'Key' type to the constructor
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +13,76 @@ class Auth_Page extends StatelessWidget {
       builder: (context, snapshot) {
         //user is logged in
         if (snapshot.hasData) {
-          return const Navigate();
+          return Navigate(); // Change 'const Navigate()' to 'Navigate()'
         } else {
           return LoginOrRegister();
         }
       },
+    );
+  }
+}
+
+class Navigate extends StatefulWidget {
+  @override
+  _NavigateState createState() => _NavigateState();
+}
+
+class _NavigateState extends State<Navigate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Study Buddy'),
+        actions: [
+          // Add any app-specific actions to the AppBar
+        ],
+      ),
+      body: _buildMainInterfaceContent(),
+      // ... Add any other necessary components like bottom navigation bar, drawer, etc.
+    );
+  }
+
+  Widget _buildMainInterfaceContent() {
+    // Create the main interface content here, such as a list of subjects or courses,
+    // recommended lessons, or any other educational material relevant to your app.
+    // Use ListView, GridView, or other widgets to present the content in an organized manner.
+
+    return ListView.builder(
+      itemCount: 10, // Replace with the actual number of items in your content list
+      itemBuilder: (context, index) {
+        final subjectId = 'subject_${index + 1}';
+        return ListTile(
+          title: Text('Subject ${index + 1}'),
+          onTap: () {
+            // Code to navigate to a specific subject's details page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SubjectDetailsPage(subjectId: subjectId)),
+            );
+          },
+          // ... Add any other content-specific widgets here
+        );
+      },
+    );
+  }
+}
+
+// Add the 'SubjectDetailsPage' class definition here
+class SubjectDetailsPage extends StatelessWidget {
+  final String subjectId;
+
+  SubjectDetailsPage({required this.subjectId});
+
+  @override
+  Widget build(BuildContext context) {
+    // Implement the UI for displaying subject details using the subjectId
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Subject Details'),
+      ),
+      body: Center(
+        child: Text('Subject ID: $subjectId'),
+      ),
     );
   }
 }
--- lib/screens/body/splash.dart
+++ lib/screens/body/splash.dart
@@ -1,88 +1,115 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-import 'package:firetrial/authentication/auth.dart';
-import 'package:flutter/material.dart'; 
-import 'dart:async'; // Add this import for Timer 
- 
-void main() { 
-  runApp(MyApp()); 
-} 
- 
-class MyApp extends StatelessWidget { 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      debugShowCheckedModeBanner: false, 
-      title: 'Study Buddy', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-        visualDensity: VisualDensity.adaptivePlatformDensity, 
-      ), 
-      home: SplashPage(), 
-    ); 
-  } 
-} 
- 
-class SplashPage extends StatefulWidget { 
-  @override 
-  _SplashPageState createState() => _SplashPageState(); 
-} 
- 
-class _SplashPageState extends State<SplashPage> { 
-  @override 
-  void initState() { 
-    super.initState(); 
-    _startTimer(); // Call the function to start the timer 
-  } 
- 
-  // Function to navigate to the main widget after the specified duration 
-  void _startTimer() { 
-    const splashDuration = Duration(seconds: 3); // Adjust the duration as needed 
- 
-    Timer(splashDuration, () { 
-      Navigator.pushReplacement( 
-        context, 
-        MaterialPageRoute(builder: (context) => const Auth_Page()), 
-      ); // Replace current screen 
-    }); 
-  } 
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      backgroundColor: Colors.blue, 
-      body: Center( 
-        child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [ 
-            Text( 
-              'Study Buddy', 
-              style: TextStyle( 
-                fontSize: 36, 
-                color: Colors.purple, 
-                fontWeight: FontWeight.bold, 
-              ), 
-            ), 
-            SizedBox(height: 16), 
-            CircularProgressIndicator( 
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green), 
-            ), 
-          ], 
-        ), 
-      ), 
-    ); 
-  } 
-} 
- 
-//class MainWidget extends StatelessWidget { 
- // @override 
-  //Widget build(BuildContext context) { 
-    //return Scaffold( 
-      //appBar: AppBar( 
-        //title: Text('Main Widget'), 
-      //), 
-      //body: Center( 
-       // child: Text('Welcome to the Main Widget!'), 
-     // ), 
-    //); 
-  //} 
-//}
+import '../../authentication/auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Study Buddy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+
+    // Initialize animation controller and fade animation
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2), // Adjust the duration as needed
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_animationController);
+
+    // Start the animation
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    const splashDuration = Duration(seconds: 3);
+
+    Timer(splashDuration, () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Auth_Page()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: GestureDetector(
+                onTap: () {
+                  // Respond to tap on the logo or character
+                  // For example, you can make it bounce or navigate to a specific page
+                },
+                child: Image.asset(
+                  'assets/app_logo.png', // Replace 'assets/app_logo.png' with your logo image asset path
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Study Buddy',
+              style: TextStyle(
+                fontSize: 36,
+                color: Colors.purple,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<Animation<double>>('_fadeAnimation', _fadeAnimation));
+  }
+}
