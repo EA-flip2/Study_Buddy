f424eab107187c9841890103e7585e18c829731e
Millie010 <98983021+Millie010@users.noreply.github.com>
2023-08-03 10:47:09 +0000

    newfeatures

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firetrial/screens/body/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:firetrial/newfeatures/pomodoro.dart';
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.android,
