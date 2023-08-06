c6684bdccedf57c9339df95169af4947cc8bbadc
EA-flip2 <91549472+EA-flip2@users.noreply.github.com>
2023-08-06 10:12:07 +0000

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:firetrial/authentication/auth.dart';
 import 'package:firetrial/screens/body/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firetrial/newfeatures/pomodoro.dart';
+
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.android,
@@ -23,21 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: Auth_Page(),
+      home: SplashPage(),
     );
-  
   }
 }
-@override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      debugShowCheckedModeBanner: true, 
-      title: 'Study Buddy', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-        visualDensity: VisualDensity.adaptivePlatformDensity, 
-      ), 
-      home: SplashPage(), 
-    ); 
-  } 
-
--- lib/screens/body/splash.dart
+++ lib/screens/body/splash.dart
@@ -1,85 +1,55 @@
+import 'package:firetrial/authentication/auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:async'; // Add this import for Timer
 
-import 'package:flutter/material.dart'; 
-import 'dart:async'; // Add this import for Timer 
- 
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
 
- 
-class MyApp extends StatelessWidget { 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      debugShowCheckedModeBanner: true, 
-      title: 'Study Buddy', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-        visualDensity: VisualDensity.adaptivePlatformDensity, 
-      ), 
-      home: SplashPage(), 
-    ); 
-  } 
-} 
- 
-class SplashPage extends StatefulWidget { 
-  @override 
-  _SplashPageState createState() => _SplashPageState(); 
-} 
- 
-class _SplashPageState extends State<SplashPage> { 
-  @override 
-  void initState() { 
-    super.initState(); 
-    _startTimer(); // Call the function to start the timer 
-  } 
- 
-  // Function to navigate to the main widget after the specified duration 
-  void _startTimer() { 
-    const splashDuration = Duration(seconds: 3); // Adjust the duration as needed 
- 
-    Timer(splashDuration, () { 
-      Navigator.pushReplacement( 
-        context, 
-        MaterialPageRoute(builder: (context) => MainWidget()), 
-      ); // Replace current screen 
-    }); 
-  } 
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      backgroundColor: Colors.blue, 
-      body: Center( 
-        child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [ 
-            Text( 
-              'Study Buddy', 
-              style: TextStyle( 
-                fontSize: 36, 
-                color: Colors.purple, 
-                fontWeight: FontWeight.bold, 
-              ), 
-            ), 
-            SizedBox(height: 16), 
-            CircularProgressIndicator( 
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green), 
-            ), 
-          ], 
-        ), 
-      ), 
-    ); 
-  } 
-} 
- 
-class MainWidget extends StatelessWidget { 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      appBar: AppBar( 
-        title: Text('Main Widget'), 
-      ), 
-      body: Center( 
-        child: Text('Welcome to the Main Widget!'), 
-      ), 
-    ); 
-  } 
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _startTimer(); // Call the function to start the timer
+  }
+
+  // Function to navigate to the main widget after the specified duration
+  void _startTimer() {
+    const splashDuration =
+        Duration(seconds: 3); // Adjust the duration as needed
+
+    Timer(splashDuration, () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Auth_Page()),
+      ); // Replace current screen
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Study Buddy',
+              style: TextStyle(
+                fontSize: 36,
+                color: Colors.purple,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
